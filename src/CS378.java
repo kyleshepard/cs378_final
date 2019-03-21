68b2f2a44f02b9a0929d2e6b32a656358cb37d89
Kyle Shepard <kshepard379@gmail.com>
2019-03-20 18:16:46 -0700

    Prototype save feature using serialization

--- src/CS378.java
+++ src/CS378.java
@@ -1,8 +1,12 @@
+import java.io.*;
+
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -11,6 +15,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class CS378 {
 	public static int[] xRes = {1920, 1280, 1024};
@@ -22,6 +31,9 @@ public class CS378 {
 	public static String curdir = System.getProperty("user.dir");
 	
 	private static JFrame frame;
+	private JTextField Name;
+	private JTextField Level;
+	private JTextField QuestsCompleted;
 
 	/**
 	 * Launch the application.
@@ -60,7 +72,7 @@ public class CS378 {
 			frame.setUndecorated(true);
 		}
 
-		JPanel gameWindow = new JPanel() {
+		JPanel background = new JPanel() {
 			/*@Override
 			  protected void paintComponent(Graphics g) {
 				ImageIcon icon = new ImageIcon(curdir + "/assets/teddy.jpg");
@@ -70,13 +82,102 @@ public class CS378 {
 			        g.drawImage(bkg, 0, 0, null);
 			}*/
 		};
-		gameWindow.setBounds(0, 0, 297, 125);
-		gameWindow.setBackground(new Color(33,33,33));
-		gameWindow.setPreferredSize(new Dimension(xRes[res],yRes[res]));
-		frame.getContentPane().add(gameWindow);
+		background.setBounds(0, 0, xRes[res], yRes[res]);
+		background.setBackground(new Color(33,33,33));
+		background.setPreferredSize(new Dimension(xRes[res],yRes[res]));
+		frame.getContentPane().add(background);
+		background.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel backgroundLabel = new JLabel(resizeIcon(new ImageIcon(curdir + "/assets/teddy.jpg"),xRes[res],yRes[res]));
-		gameWindow.add(backgroundLabel);
+		JLabel backgroundLabel = new JLabel(resizeIcon(new ImageIcon(curdir + "/assets/testbkg.jpg"),xRes[res],yRes[res]));
+		background.add(backgroundLabel);
+		
+		JPanel UIPanel = new JPanel() {
+		};
+		//UIPanel.setVisible(false);
+		UIPanel.setPreferredSize(new Dimension(xRes[res],yRes[res]));
+		//UIPanel.setBackground(new Color(33, 33, 33));
+		frame.getContentPane().add(UIPanel, BorderLayout.CENTER);
+		UIPanel.setLayout(null);
+		
+		JButton btnSave = new JButton("save");
+		btnSave.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Save save1 = new Save(Name.getText(),Integer.parseInt(Level.getText()), Integer.parseInt(QuestsCompleted.getText()));
+		        String curdir = System.getProperty("user.dir");
+
+		        try {
+		            FileOutputStream fileOut =
+		            new FileOutputStream(curdir + "save.dat");
+		            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		            out.writeObject(save1);
+		            out.close();
+		            fileOut.close();
+		            System.out.printf("Serialized data is saved in save.dat");
+		         } catch (IOException i) {
+		            i.printStackTrace();
+		         }
+				
+			}
+		
+		});
+		btnSave.setBounds(0, 0, 75, 23);
+		UIPanel.add(btnSave);
+		
+		JButton btnLoad = new JButton("load");
+		btnLoad.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Save save1 = null;
+			      String curdir = System.getProperty("user.dir");
+			      try {
+			         FileInputStream fileIn = new FileInputStream(curdir + "save.dat");
+			         ObjectInputStream in = new ObjectInputStream(fileIn);
+			         save1 = (Save) in.readObject();
+			         in.close();
+			         fileIn.close();
+			      } catch (IOException i) {
+			         i.printStackTrace();
+			         return;
+			      } catch (ClassNotFoundException c) {
+			         System.out.println("Employee class not found");
+			         c.printStackTrace();
+			         return;
+			      }
+			      
+			      System.out.println("Deserialized Save...");
+			      System.out.println("Name: " + save1.name);
+			      System.out.println("Level: " + save1.level);
+			      System.out.println("Health: " + save1.health);
+			      System.out.println("Location: [" + save1.xPos + "][" + save1.yPos + "]");
+			      System.out.println("Quests Completed: " + save1.questsCompleted);
+			      System.out.println("Inventory: ");
+			      for ( String s : save1.inventory){
+			          System.out.print(s + " ");
+			      }
+				
+			}
+		
+		});
+		btnLoad.setBounds(75, 0, 75, 23);
+		UIPanel.add(btnLoad);
+		
+		Name = new JTextField();
+		Name.setBounds(0, 25, 100, 25);
+		UIPanel.add(Name);
+		Name.setColumns(10);
+		
+		Level = new JTextField();
+		Level.setColumns(10);
+		Level.setBounds(0, 50, 100, 25);
+		UIPanel.add(Level);
+		
+		QuestsCompleted = new JTextField();
+		QuestsCompleted.setColumns(10);
+		QuestsCompleted.setBounds(0, 75, 100, 25);
+		UIPanel.add(QuestsCompleted);
 		//backgroundLabel.setPreferredSize(new Dimension(xRes[res],yRes[res]));
 		
 		
@@ -85,8 +186,7 @@ public class CS378 {
 	
 	private ImageIcon resizeIcon(ImageIcon icon, int xSize, int ySize) {
 		Image img = icon.getImage();
-		img = img.getScaledInstance(xSize, ySize,  java.awt.Image.SCALE_FAST);
+		img = img.getScaledInstance(xSize, ySize,  java.awt.Image.SCALE_SMOOTH);
 		return new ImageIcon(img); 
 	}
-	
 }
---
+++ src/Save.java
@@ -0,0 +1,20 @@
+public class Save implements java.io.Serializable{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public String name = "Default";
+    public int level = 0;
+    public int health = 100;
+    public int xPos = 0;
+    public int yPos = 0;
+    public String[] inventory = {"Potion","Sword of Truth","10 Pesos"};
+    public int questsCompleted = 0;
+
+    public Save(String name, int level, int questsCompleted){
+    	this.name = name;
+    	this.level = level;
+    	this.questsCompleted = questsCompleted;
+    }
+
+}
