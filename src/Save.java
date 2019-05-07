2dc8a9166c26b1eb548558ad5982dcd6cf7d4342
Kyle Shepard <kshepard379@gmail.com>
2019-05-07 14:05:00 -0700

    simplified variables for save file

--- src/Save.java
+++ src/Save.java
@@ -1,22 +1,12 @@
 public class Save implements java.io.Serializable{
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	public String name = "Default";
-    public int level = 0;
-    public int health = 100;
-    public int xPos = 0;
-    public int yPos = 0;
-    public String[] inventory = {"Potion","Sword of Truth","10 Pesos"};
-    public int questsCompleted = 0;
-    public Room curr;
 
-    public Save(String name, int level, int questsCompleted,Room curr){
-    	this.name = name;
-    	this.level = level;
-    	this.questsCompleted = questsCompleted;
-    	this.curr = curr;
+	private static final long serialVersionUID = 1847299946633584979L;
+	private Player p;
+    private Room currentRoom;
+
+    public Save(Player p,Room currentRoom){
+    	this.p = p;
+    	this.currentRoom = currentRoom;
     }
     
     public Save() {
@@ -24,7 +14,7 @@ public class Save implements java.io.Serializable{
     }
     
     public Room getCurrentRoom(){
-    	return curr;
+    	return currentRoom;
     }
 
 }
