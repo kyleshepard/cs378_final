88292d8a3b222213a78077b7c20d9ac6d1f98abb
SgtBacon <45608814+SgtBacon@users.noreply.github.com>
2019-05-15 16:51:06 -0700

    Added comments

--- src/QuestGiver.java
+++ src/QuestGiver.java
@@ -4,11 +4,11 @@ public class QuestGiver extends Entity{
 		
 	}
 	
-	public QuestGiver(int ID, String name, int health, String sprite, int strength) {
+	public QuestGiver(int ID, String name, int health, String sprite, int strength) {//Base constructor
 		super(ID, name, health, sprite, strength);
 	}
 	
-	public void giveQuest(Quest item){
+	public void giveQuest(Quest item){	//Lets QuestGivers add a Quest to the Player's vector of Quests
 	Player.addQuest(item);
 	}
 	
@@ -19,12 +19,12 @@ public class QuestGiver extends Entity{
 	
 	@Override
 	public void onClick(){
-		if(getName().equals("Sheriff")) {
-			if(Player.searchQuest(123) == false) {
+		if(getName().equals("Sheriff")) {	//Checks if the ClickableObject is the Sheriff
+			if(Player.searchQuest(123) == false) {	//If the Player does not have the KillQuest, open up the dialogue box used to give it to them
 				CS378.DialogueBox.setEnabled(true);
 			}
 			else {
-				CS378.FinishQuest.setEnabled(true);
+				CS378.FinishQuest.setEnabled(true);	//If the Player does have the KillQuest, open up the FinishQuest Menu used to complete it
 			}
 		}
 	
